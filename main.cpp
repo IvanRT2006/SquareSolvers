adb2c6f0cf4a55884667f636e42cacadac24bfe1
IvanRT2006 <ivanryabov06@mail.ru>
2024-08-13 14:52:59 +0300

    Fix codestyle and add compare double func

--- main.cpp
+++ main.cpp
@@ -1,80 +1,119 @@
 #include "TxLib.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
-void LinealEquation(float b, float c);
 
-void QuadroEquation(float a, float b, float c);
+const double eps = 1e-9;
+
+void LinealEquation(double b, double c);
+void QuadroEquation(double a, double b, double c);
+bool CompareEqual(double a, double b);
+bool CompareMore(double a, double b);
 
 int main()
 {
-
-    float a=0.5;
-
-    float b=0.5;
-
-    float c=0.5;
+    double a = 0;
+    double b = 0;
+    double c = 0;
 
     printf("Enter the coefficients of quadratic equation:\n");
 
-    while(scanf("%f %f %f",&a,&b,&c)==3)
+    while (scanf("%lf %lf %lf",&a, &b, &c)==3)
     {
-        if (a==0)
+        if (CompareEqual(a,0) == true)
         {
-            LinealEquation(b,c);
-
+            LinealEquation(b, c);
             continue;
         }
         else
         {
-            QuadroEquation(a,b,c);
-
+            QuadroEquation(a, b, c);
             continue;
         }
     }
+
     printf("Program ended.");
 
     return 0;
 }
 
-void LinealEquation(float b, float c)
+void LinealEquation(double b, double c)
 {
-    if((b!=0)&&(c!=0))
-
-        printf("Equation isn't quadro. Root only one %f\n", -c/b);
-
-    else if((b==0)&&(c==0))
-
-        printf("There isn't a border in solving this equation\n");
-
-    else if((b==0)&&(c!=0))
-
-        printf("There aren't any solvers in this equation\n");
-
-    else
-        printf("Equation isn't quadro. Root only one equal 0\n");
-
-    printf("Enter the other coefficients of quadratic equation:\n");
-}
-
-void QuadroEquation(float a, float b, float c)
-{
-    float D = b*b-4*a*c;
-
-    if(D<0)
+    if (CompareEqual(b, 0) == true)
     {
-        printf("oh,no!\n");
+        if (CompareEqual(c, 0) == true)
+        {
+            printf("Solvers in this equation don't have borders\n");
+        }
 
-        printf("It equation doesn't have roots.\n");
-
+        else
+        {
+            printf("Not solvers in this equation\n");
+        }
     }
-    else if (D==0)
-
-       printf("only one square:%f\n",-b/(2*a));
 
     else
+    {
+        printf("Lineal Equation! Only one root %lf\n", -c/b);
+    }
 
-        printf("two squares: %f %f\n",(-b+sqrt(D))/(2*a),(-b-sqrt(D))/(2*a));
-
-    printf("Enter the other coefficients of quadratic equation:\n");
+    printf("Enter other coefficients:\n");
 }
+
+void QuadroEquation(double a, double b, double c)
+{
+    const double D = b * b - 4 * a * c;
+
+    if (CompareEqual(D, 0) == true)
+    {
+        const double only_one_root = -b / (2 * a);
+
+        printf("only one root %lf\n", only_one_root);
+    }
+
+    else
+    {
+        if (CompareMore(D, 0) == true)
+        {
+            const double first_root = (-b + sqrt(D)) / (2 * a);
+            const double second_root = (-b - sqrt(D)) / (2 * a);
+
+            printf("Two roots: %lf %lf\n", first_root, second_root);
+        }
+
+        else
+        {
+            printf("Oh no!Not roots\n");
+        }
+    }
+
+    printf("Enter other coefficients:\n");
+}
+
+bool CompareEqual(double a, double b)
+{
+    if (abs(a - b) < eps)
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+bool CompareMore(double a, double b)
+{
+    if ((a - b) > eps)
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
